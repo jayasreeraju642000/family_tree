7bcf23bc912f5ce040f55982aeb665552427198a
jayasreeraju642000 <125048048+jayasreeraju642000@users.noreply.github.com>
2024-01-29 15:30:22 +0530

    added familyTreeBloc

--- lib/family_tree.dart
+++ lib/family_tree.dart
@@ -2,6 +2,7 @@ library family_tree;
 
 export 'package:family_tree/family_tree.dart';
 export 'package:family_tree/widgets/family_node_widget.dart';
+export 'package:family_tree/bloc/family_tree/family_tree_bloc.dart';
 
 import 'package:family_tree/bloc/family_tree/family_tree_bloc.dart';
 import 'package:family_tree/widgets/family_node_widget.dart';
@@ -18,61 +19,66 @@ class FamilyTree extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    context.read<FamilyTreeBloc>().add(FamilyTreeLoadingEvent());
+    return BlocProvider(
+      create: (context) => FamilyTreeBloc(),
+      child: Builder(builder: (context) {
+        context.read<FamilyTreeBloc>().add(FamilyTreeLoadingEvent());
 
-    return Scaffold(
-      key: homePageKey,
-      body: BlocConsumer<FamilyTreeBloc, FamilyTreeState>(
-        listener: (context, state) {},
-        builder: (context, state) {
-          if (state is FamilyTreeLoaded) {
-            return InteractiveViewer(
-                constrained: false,
-                minScale: 0.0001,
-                maxScale: 8.0,
-                child: Container(
-                  padding: EdgeInsets.only(
-                      top: state.verticalGap / 2,
-                      left: state.horizontalGap / 2),
-                  width: MediaQuery.of(context).size.width,
-                  child: SingleChildScrollView(
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: SizedBox(
-                        width: state.viewPortWidth == 0.0
-                            ? MediaQuery.of(context).size.width
-                            : state.viewPortWidth,
-                        height: state.viewPortHeight == 0.0
-                            ? MediaQuery.of(context).size.height
-                            : state.viewPortHeight,
-                        child: Stack(
-                          children: [
-                            FamilyTreeDrawing(
-                              nodes: state.nodes,
-                              horizontalGap: state.horizontalGap,
-                              nodeHeight: state.heightOfNode,
-                              nodeWidth: state.widthOfNode,
-                              verticalGap: state.verticalGap,
-                              height: state.viewPortHeight,
-                              width: state.viewPortWidth,
+        return Scaffold(
+          key: homePageKey,
+          body: BlocConsumer<FamilyTreeBloc, FamilyTreeState>(
+            listener: (context, state) {},
+            builder: (context, state) {
+              if (state is FamilyTreeLoaded) {
+                return InteractiveViewer(
+                    constrained: false,
+                    minScale: 0.0001,
+                    maxScale: 8.0,
+                    child: Container(
+                      padding: EdgeInsets.only(
+                          top: state.verticalGap / 2,
+                          left: state.horizontalGap / 2),
+                      width: MediaQuery.of(context).size.width,
+                      child: SingleChildScrollView(
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: SizedBox(
+                            width: state.viewPortWidth == 0.0
+                                ? MediaQuery.of(context).size.width
+                                : state.viewPortWidth,
+                            height: state.viewPortHeight == 0.0
+                                ? MediaQuery.of(context).size.height
+                                : state.viewPortHeight,
+                            child: Stack(
+                              children: [
+                                FamilyTreeDrawing(
+                                  nodes: state.nodes,
+                                  horizontalGap: state.horizontalGap,
+                                  nodeHeight: state.heightOfNode,
+                                  nodeWidth: state.widthOfNode,
+                                  verticalGap: state.verticalGap,
+                                  height: state.viewPortHeight,
+                                  width: state.viewPortWidth,
+                                ),
+                                for (var node in state.nodes)
+                                  FamilyNodeWidget(
+                                    node: node,
+                                    widthOfNode: state.widthOfNode,
+                                    heightOfNode: state.heightOfNode,
+                                  ),
+                              ],
                             ),
-                            for (var node in state.nodes)
-                              FamilyNodeWidget(
-                                node: node,
-                                widthOfNode: state.widthOfNode,
-                                heightOfNode: state.heightOfNode,
-                              ),
-                          ],
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                ));
-          } else {
-            return const Center(child: Text('Loading.....'));
-          }
-        },
-      ),
+                    ));
+              } else {
+                return const Center(child: Text('Loading.....'));
+              }
+            },
+          ),
+        );
+      }),
     );
   }
 }
